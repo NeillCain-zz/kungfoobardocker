1a5cf5308d6483319893b5dfa600b1b2cadb394d
Neill Cain <n.cain@craneware.com>
2016-05-11 13:43:58 +0100

    non ES6

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 'use strict';
 
-const http = require('http');
-const ip = require('ip');
-const PORT = 8080;
+var http = require('http');
+var ip = require('ip');
+var PORT = 8080;
 
-const server = http.createServer(function(request, response) {
+var server = http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/html"});
     response.write('Hello from server at ' + ip.address() + '!!!');
       response.end();
 });
 
-server.listen(PORT, () => {
+server.listen(PORT, function(){
     console.log('server loaded at ' + ip.address());
 });
